33eab118053c12586f193971ce1893aca9f130c0
cgehy <ghybaoxue@163.com>
2024-09-03 11:55:00 +0800

    Create TTSWebSocketRecognitionSynthesis.cpp

---
+++ GPTSoVITS/Private/WebSocket/TTSWebSocket/TTSWebSocketRecognitionSynthesis.cpp
@@ -0,0 +1,26 @@
+#include "WebSocket/TTSWebSocket/TTSWebSocketRecognitionSynthesis.h"
+
+void FTTSWebSocketRecognitionSynthesis::OnMessage(const FString& InMsg)
+{
+	
+}
+
+void FTTSWebSocketRecognitionSynthesis::OnMessageSent(const FString& InMsg)
+{
+}
+
+void FTTSWebSocketRecognitionSynthesis::OnClosed(int32 InStatusCode, const FString& InReason, bool bWasClean)
+{
+}
+
+void FTTSWebSocketRecognitionSynthesis::OnConnectionError(const FString& InError)
+{
+}
+
+void FTTSWebSocketRecognitionSynthesis::OnConnected()
+{
+}
+
+void FTTSWebSocketRecognitionSynthesis::OnRawMessage(const void* InData, SIZE_T InSize, SIZE_T InBytesRemaining)
+{
+}
