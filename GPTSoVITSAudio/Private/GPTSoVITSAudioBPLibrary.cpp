86fe8aa41536b7f908984a83a5f311e40deffbce
cgehy <ghybaoxue@163.com>
2024-09-03 11:55:46 +0800

    Create GPTSoVITSAudioBPLibrary.cpp

---
+++ GPTSoVITSAudio/Private/GPTSoVITSAudioBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GPTSoVITSAudioBPLibrary.h"
+#include "GPTSoVITSAudio.h"
+
+UGPTSoVITSAudioBPLibrary::UGPTSoVITSAudioBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UGPTSoVITSAudioBPLibrary::GPTSoVITSAudioSampleFunction(float Param)
+{
+	return -1;
+}
+
